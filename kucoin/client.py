870011677cc5de3f3eb089efc63de39994f7e3b1
nielskool <nielskool@live.nl>
2019-02-23 20:02:45 +0100

    Add fiat price

--- kucoin/client.py
+++ kucoin/client.py
@@ -1396,6 +1396,15 @@ class Client(object):
             return self._get('market/orderbook/level1', False, data=data)
         else:
             return self._get('market/allTickers', False)
+        
+    def get_fiat_price(self, base='USD', symbol=None):
+        
+        data = {
+                'base': base,
+                'symbol': symbol
+            }
+        
+        return self._get('market/allTickers', False, data=data)
 
     def get_24hr_stats(self, symbol):
         """Get 24hr stats for a symbol. Volume is in base currency units. open, high, low are in quote currency units.
